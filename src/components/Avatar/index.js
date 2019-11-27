7c20af1ea5018a8a5e1251374fe30804d1e57602
Igor Trindade <igorhideki@gmail.com>
2019-11-27 11:18:48 -0300

    Add component avatar

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function AvatarInput() {
+  return (
+    <Container>
+      <img src="https://api.adorable.io/avatars/150/eightone" alt="profile" />
+    </Container>
+  );
+}
---
+++ src/components/Avatar/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  img {
+    width: 70px;
+    height: 70px;
+    min-width: 70px;
+    min-height: 70px;
+    border-radius: 50%;
+    border: 4px solid #fff;
+    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.1);
+    background: #eee;
+  }
+`;
